71f2c1759a91fa722568001855cf70b1633df832
Nos_ave <nos@ik.me>
2026-04-13 16:55:26 +0200

    Explication Transorm

---
+++ Assets/Project/Script/MyFirstScript.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class MyFirstScript : MonoBehaviour
+{
+    private Transform monTransform;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        monTransform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        monTransform.position += new Vector3(0.01f,0 , 0);
+        GetComponent<Transform>().position = GetComponent<Transform>().position + new Vector3(0.01f, 0, 0);
+    }
+}
