27673ecc9c3c39bd66238b9f07a51f31287cfb27
Nos_ave <nos@ik.me>
2026-04-14 16:45:17 +0200

    [DEV] Fix void Start, Update and SpawnPipe

--- Assets/Project/Script/PipeSpawner.cs
+++ Assets/Project/Script/PipeSpawner.cs
@@ -9,12 +9,12 @@ namespace Project.Script
         public float spawnRate = 1f;
     
         // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
+        private void Start()
         {
             InvokeRepeating(nameof(SpawnPipe), 1f, spawnRate);
         }
 
-        void SpawnPipe()
+        private void SpawnPipe()
         {
             float randomY = Random.Range(-1f, 1f);
             Vector3 spawnPosY = new Vector3(transform.position.x, randomY, 0);
@@ -23,7 +23,7 @@ namespace Project.Script
         }
 
         // Update is called once per frame
-        void Update()
+        private void Update()
         {
         
         }
