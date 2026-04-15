082f8f6fec9be6d6b81781a17d34ba12aa6aaa29
Nos_ave <nos@ik.me>
2026-04-15 11:40:35 +0200

    [DEV] Add scripts and GameObjects

--- Assets/Project/Script/TriggerScript.cs
+++ Assets/Project/Script/TriggerScript.cs
@@ -4,22 +4,9 @@ namespace Project.Script
 {
     public class TriggerScript : MonoBehaviour
     {
-        void OnTriggerEnter2D(Collider2D other)
+        private void OnTriggerEnter2D(Collider2D other)
         {
             Debug.Log("Trigger detected");
         }
-    
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
-        {
-        
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-        
-        }
-    
     }
 }
